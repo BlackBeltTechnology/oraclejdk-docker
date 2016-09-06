284b1844d62e76a8215ca511d4d2c0d69297f7d3
József Börcsök <jozsef.borcsok@avon.com>
2016-09-06 11:14:25 +0200

    update build script: download Maven if not available, do not run tests

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 # based on https://hub.docker.com/r/frolvlad/alpine-oraclejdk8/
 #docker build -t $PREFIX/alpine-glibc alpine-glibc
 
@@ -10,12 +8,17 @@ PREFIX=${DOCKER_PREFIX:-blackbelt}
 CWD=`dirname $0`
 ARTIFACTS_DIR="${CWD}/artifacts"
 
+MAVEN_COMMAND='mvn'
+MAVEN_MAJOR=3
+MAVEN_VERSION=3.3.9
+
 function check_command {
     which "$1" > /dev/null 2>&1
     if [ "$?" -ne 0 ]
     then
-        echo -e "Command '$1' should be installed and added to PATH"
-        exit 1
+        wget -O /tmp/apache-maven.tar.gz https://www.apache.org/dist/maven/maven-${MAVEN_MAJOR}/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
+        tar xf /tmp/apache-maven.tar.gz -C /tmp
+        MAVEN_COMMAND="`ls -ad /tmp/apache-maven*/`bin/mvn"
     fi
 }
 
@@ -23,7 +26,11 @@ check_command mvn
 
 mkdir -p "${ARTIFACTS_DIR}"
 
-#mvn clean install -f "${CWD}/jcollectd/pom.xml"
+set -e
+
+${MAVEN_COMMAND} clean install -Dmaven.test.skip=true -f "${CWD}/jcollectd/pom.xml"
+
+rm -Rf /tmp/apache-maven*
 
 cp ${CWD}/jcollectd/target/jcollectd-*.jar "${ARTIFACTS_DIR}/jcollectd.jar"
 tar cvzf "${ARTIFACTS_DIR}/jcollectd-conf.tar.gz" -C "${CWD}/jcollectd/etc/" .
@@ -41,3 +48,4 @@ function build_oracle_jdk {
 }
 
 build_oracle_jdk $@
+
